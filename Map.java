af2eddee9d21cea0e6ef0747f51c906a3c8387e9
Mr. Lamont <adam_lamont@wrdsb.ca>
2024-03-19 07:52:11 -0400

    Map and Scene changes

--- Map.java
+++ Map.java
@@ -9,16 +9,87 @@ public class Map {
     private Scene currentScene;
 
     public Map(String filename){
-       
+        // create the Scanner for text input
+        Scanner input = null;
+        try{
+            input = new Scanner(new File(filename));
+        }catch(Exception e){
+            // print any errors
+            e.printStackTrace();
+        }
+        // get the size of the map
+        int numRows = input.nextInt();
+        // move down a row
+        input.nextLine();
+        int numCols = input.nextInt();
+        input.nextLine();
+
+        // scan in the starting position
+        this.currentRow = input.nextInt();
+        input.nextLine();
+        this.currentCol = input.nextInt();
+        input.nextLine();
+
+        // create the array to store the Scenes
+        this.scenes = new Scene[numRows*numCols];
+        // scan in and store each Scene
+        for(int i = 0; i < this.scenes.length; i++){
+            // image file name
+            String imageFile = input.next();
+            // can move directions
+            boolean north = input.nextBoolean();
+            boolean east = input.nextBoolean();
+            boolean south = input.nextBoolean();
+            boolean west = input.nextBoolean();
+            // description text (maybe) and move down
+            String description = input.nextLine();
+
+            // get the data from the image filename
+            // remove the .png from the filename
+            String dataString = filename.replace(".png","");
+            // break up the data based on the dashes
+            String[] data = dataString.split("-");
+            // recovering the row and col
+            int row = Integer.parseInt(data[1]);
+            int col = Integer.parseInt(data[3]);
+
+            // make the scene and store it!
+            Scene s = new Scene(row,col,imageFile,north,east,
+                                    south,west,description);
+            this.scenes[i] = s;
+
+            // set the current scene if found
+            if(row == this.currentRow && 
+                                    col == this.currentCol){
+                this.currentScene = this.scenes[i];
+            }
+        }
 
     }
 
     public Scene findScene(int row, int col){
-        
+        // go through each scene
+        for(int i = 0; i < this.scenes.length; i++){
+            // grab a scene to look at
+            Scene s = this.scenes[i];
+            // does it match the row and col?
+            if(s.getRow() == row && s.getCol() == col){
+                // found it so return it
+                return s;
+            }
+        }
+        // didn't find a scene matching that row or col
+        return null;
     }
 
     public void moveNorth(){
-       
+        // can we move north?
+        if(this.currentScene.canMoveNorth()){
+            // move row up
+            this.currentRow--;
+            //update the current scene
+            this.currentScene = findScene(currentRow, currentCol);
+        }
     }
 
     public void moveEast(){
@@ -34,11 +105,11 @@ public class Map {
     }
 
     public BufferedImage getImage(){
-       
+        return this.currentScene.getImage();
     }
 
     public String getDescription(){
-        
+        return this.currentScene.getDescription();
     }
 
 }
--- Scene.java
+++ Scene.java
@@ -12,40 +12,57 @@ public class Scene {
     private boolean moveWest;
     private String description;
 
-    public Scene(int row, int col, 
-       
+    public Scene(int row, int col, String filename, boolean moveNorth, boolean moveEast, boolean moveSouth, boolean moveWest, String description){
+       this.row = row;
+       this.col = col;
+       this.moveNorth = moveNorth;
+       this.moveEast = moveEast;
+       this.moveSouth = moveSouth;
+       this.moveWest = moveWest;
+       this.description = description;
+
+       // load in an image
+       // start off saying "no image present"
+        this.image = null;
+        // try to use ImageIO to load the image
+        try{
+            this.image = ImageIO.read(new File(filename));
+        }catch(Exception e){
+            // print out any errors
+            e.printStackTrace();
+        }
     }
 
 
     public int getRow(){
-        
+        return this.row;
     }
 
     public int getCol(){
-        
+        return this.col;
     }
 
     public BufferedImage getImage(){
-        
+        return this.image;
     }
 
     public boolean canMoveNorth(){
-       
+       return this.moveNorth;
     }
 
     public boolean canMoveEast(){
-       
+       return this.moveEast;
     }
 
     public boolean canMoveSouth(){
-        
+        return this.moveSouth;
     }
 
     public boolean canMoveWest(){
-        
+        return this.moveWest;
     }
 
     public String getDescription(){
-        
+        return this.description;
     }
 }
